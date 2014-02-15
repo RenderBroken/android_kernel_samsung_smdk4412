8c93f406890546e6c9e00ef93e1e6b2727f42a6a
Paul Reioux <reioux@gmail.com>
2014-02-14 20:06:55 -0800

    Asynchronous Fsync: initial extraction of Async Fsync from HTC
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    	fs/sync.c
    	include/linux/fs.h

--- build_10.2.sh
+++ build_10.2.sh
@@ -1,5 +1,6 @@
 rm /home/gustavo/zips/*.zip
-/home/gustavo/kernel/usr/build_scripts/build_kernel.sh
+cp /home/gustavo/kernel/arch/arm/configs/kernel_defconfig /home/gustavo/kernel/.config
+make -j3
 find -name '*.ko' -exec cp -av {} /home/gustavo/kernel/usr/galaxys2_initramfs_files/modules/ \;
 chmod 644 /home/gustavo/kernel/usr/galaxys2_initramfs_files/modules/*
 /home/gustavo/toolchain/bin/arm-linux-androideabi-strip --strip-unneeded /home/gustavo/kernel/usr/galaxys2_initramfs_files/modules/*
@@ -8,5 +9,6 @@ cp /home/gustavo/kernel/arch/arm/boot/zImage /home/gustavo/zip_tmp/
 CURRENTDATE=$(date +"%d-%m")
 cd /home/gustavo/zip_tmp
 rm *.zip
-zip -r cm-10.2-kernel-$CURRENTDATE-CWM.zip ./
-cp ./*.zip /home/gustavo/zips/cm-10.2-kernel-$CURRENTDATE-CWM.zip
+zip -r kk-kernel-$CURRENTDATE-CWM.zip ./
+cp ./*.zip /home/gustavo/zips/jb-kernel-$CURRENTDATE-CWM.zip
+cd /home/gustavo/kernel
