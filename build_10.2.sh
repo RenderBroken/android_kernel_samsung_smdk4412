38c7283eed79f86bac347a726966726ce9ff9592
Gustavo Santos <gustavo.santos0106@gmail.com>
2014-02-26 00:02:45 -0300

    cpufreq: remove old undervolting settings

--- build_10.2.sh
+++ build_10.2.sh
@@ -3,7 +3,7 @@ cp /home/gustavo/kernel/arch/arm/configs/kernel_defconfig /home/gustavo/kernel/.
 make -j3
 find -name '*.ko' -exec cp -av {} /home/gustavo/kernel/usr/galaxys2_initramfs_files/modules/ \;
 chmod 644 /home/gustavo/kernel/usr/galaxys2_initramfs_files/modules/*
-/home/gustavo/toolchain/bin/arm-linux-androideabi-strip --strip-unneeded /home/gustavo/kernel/usr/galaxys2_initramfs_files/modules/*
+/home/gustavo/toolchain/bin/arm-gnueabi-strip --strip-unneeded /home/gustavo/kernel/usr/galaxys2_initramfs_files/modules/*
 make -j3 zImage CONFIG_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/initramfs/cm-10.2_initramfs.list"
 cp /home/gustavo/kernel/arch/arm/boot/zImage /home/gustavo/zip_tmp/
 CURRENTDATE=$(date +"%d-%m")
