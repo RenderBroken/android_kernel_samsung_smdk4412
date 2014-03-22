f8c4a77e93e942748b0fdf0e93e9d88fb2818ffd
Zachariah Kennedy <zkennedy87@gmail.com>
2014-03-22 14:10:59 -0500

    Build: Setup build script

--- build.sh
+++ build.sh
@@ -1,76 +1,65 @@
 #!/bin/bash
 
-TOOLCHAIN="/home/gustavo/toolchain/bin/arm-cortex_a9-linux-gnueabihf-"
-STRIP="/home/gustavo/toolchain/bin/arm-gnueabi-strip"
+TOOLCHAIN="/media/storage/android/source/toolchains/arm-cortex_a9-linux-gnueabihf-linaro_4.7.4-2014.01/bin/arm-cortex_a9-linux-gnueabihf-"
+STRIP="/media/storage/android/source/toolchains/arm-cortex_a9-linux-gnueabihf-linaro_4.7.4-2014.01/bin/arm-gnueabi-strip"
 OUTDIR="out"
-KK_CWM_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/initramfs/cwm.list"
-KK_TWRP_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/initramfs/twrp.list"
-JB_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/initramfs/jb.list"
-RAMDISK="/home/gustavo/ramdisk"
-RAMDISK_OUT="/home/gustavo/kernel/usr/initramfs/ramdisk.cpio"
-MODULES=("/home/gustavo/kernel/net/sunrpc/auth_gss/auth_rpcgss.ko" "/home/gustavo/kernel/fs/cifs/cifs.ko" "drivers/net/wireless/bcmdhd/dhd.ko" "/home/gustavo/kernel/fs/lockd/lockd.ko" "/home/gustavo/kernel/fs/nfs/nfs.ko" "/home/gustavo/kernel/net/sunrpc/auth_gss/rpcsec_gss_krb5.ko" "drivers/scsi/scsi_wait_scan.ko" "drivers/samsung/fm_si4709/Si4709_driver.ko" "/home/gustavo/kernel/net/sunrpc/sunrpc.ko")
-KERNEL_DIR="/home/gustavo/kernel"
-MODULES_DIR="/home/gustavo/kernel/usr/galaxys2_initramfs_files/modules"
-CURRENTDATE=$(date +"%d-%m")
+CONFIG="kernel_defconfig"
+KK_CWM_INITRAMFS_SOURCE="/media/storage/android/source/smdk4412/usr/initramfs/cwm-i777.list"
+KK_TWRP_INITRAMFS_SOURCE="/media/storage/android/source/smdk4412/usr/initramfs/twrp-i777.list"
+JB_INITRAMFS_SOURCE="/media/storage/android/source/smdk4412/usr/initramfs/jb-i777.list"
+RAMDISK="/media/storage/android/source/smdk4412/ramdisk"
+RAMDISK_OUT="/media/storage/android/source/smdk4412/usr/initramfs/ramdisk-i777.cpio"
+MODULES=("/media/storage/android/source/smdk4412/net/sunrpc/auth_gss/auth_rpcgss.ko" "/media/storage/android/source/smdk4412/fs/cifs/cifs.ko" "drivers/net/wireless/bcmdhd/dhd.ko" "/media/storage/android/source/smdk4412/fs/lockd/lockd.ko" "/media/storage/android/source/smdk4412/fs/nfs/nfs.ko" "/media/storage/android/source/smdk4412/net/sunrpc/auth_gss/rpcsec_gss_krb5.ko" "drivers/scsi/scsi_wait_scan.ko" "drivers/samsung/fm_si4709/Si4709_driver.ko" "/media/storage/android/source/smdk4412/net/sunrpc/sunrpc.ko")
+KERNEL_DIR="/media/storage/android/source/smdk4412"
+MODULES_DIR="/media/storage/android/source/smdk4412/usr/galaxys2_initramfs_files/modules"
+CURRENTDATE=$(date +"%m-%d")
 
-case "$1" in
-	clean)
-        cd ${KERNEL_DIR}
-        make clean && make mrproper
-		;;
-	kk)
-        # compress the ramdisk in cpio
-        cd ${RAMDISK}
-        rm *.cpio
-        find . -not -name ".gitignore" | cpio -o -H newc > ${RAMDISK_OUT}
-        
-        cd ${KERNEL_DIR}
-        make -j3 kernel_defconfig ARCH=arm CROSS_COMPILE=${TOOLCHAIN}
 
-        # build modules first to include them into android ramdisk
-        make -j3 ARCH=arm CROSS_COMPILE=${TOOLCHAIN} modules
-       
-        for module in "${MODULES[@]}" ; do
-            cp "${module}" ${MODULES_DIR}
-            ${STRIP} --strip-unneeded ${MODULES_DIR}/*
-        done
-      
-        # build the CWM kernel
-        cd ${KERNEL_DIR}
-        make -j3 ARCH=arm CROSS_COMPILE=${TOOLCHAIN} CONFIG_INITRAMFS_SOURCE=${KK_CWM_INITRAMFS_SOURCE}
-        cp arch/arm/boot/zImage ${OUTDIR}
-        cd ${OUTDIR}
-		echo "Creating kk CWM kernel zip..."
-        zip -r kk-kernel-$CURRENTDATE-CWM.zip ./ -x *.zip /system/* *.gitignore
-        # build the TWRP kernel
-        cd ${KERNEL_DIR}
-        make -j3 ARCH=arm CROSS_COMPILE=${TOOLCHAIN} CONFIG_INITRAMFS_SOURCE=${KK_TWRP_INITRAMFS_SOURCE}
-        cp arch/arm/boot/zImage ${OUTDIR}
-        cd ${OUTDIR}
-		echo "Creating kk TWRP kernel zip..."
-        zip -r kk-kernel-$CURRENTDATE-TWRP.zip ./ -x *.zip /system/* *.gitignore
-        
-		echo "Done!"
-	    ;;
-	  jb)
-	    cd ${KERNEL_DIR}
-        make kernel_defconfig ARCH=arm CROSS_COMPILE=${TOOLCHAIN}
+#echo "Cleaning..."
+#		cd ${KERNEL_DIR}
+#		make clean && make mrproper
 
-        # build modules first to include them into android ramdisk
-        make -j3 ARCH=arm CROSS_COMPILE=${TOOLCHAIN} modules
-       
-        for module in "${MODULES[@]}" ; do
-            cp "${module}" ${MODULES_DIR}
-            ${STRIP} --strip-unneeded ${MODULES_DIR}/*
-        done
-      
-        # build the jelly bean kernel
-        cd ${KERNEL_DIR}
-        make -j3 ARCH=arm CROSS_COMPILE=${TOOLCHAIN} CONFIG_INITRAMFS_SOURCE=${JB_INITRAMFS_SOURCE} zImage
-        cp arch/arm/boot/zImage ${OUTDIR}
-        cd ${OUTDIR}
-		echo "Creating jb kernel zip..."
-        zip -r jb-kernel-$CURRENTDATE.zip ./ -x *.zip *.gitignore
-        
-        echo "Done!"
-esac
+	read -p "Clean working directory..(y/n)? : " achoice
+	case "$achoice" in
+		y|Y )
+			rm -rf arch/arm/boot/zImage
+			make clean && make mrproper
+			echo "Working directory cleaned...";;
+		n|N )
+	esac
+
+#echo "Making ramdisk..."
+#		cd ${RAMDISK}
+#		rm *.cpio
+#		find . -not -name ".gitignore" | cpio -o -H newc > ${RAMDISK_OUT}
+
+echo "Initial Build..."
+		cd ${KERNEL_DIR}
+		make ${CONFIG}
+		make -j3 ARCH=arm CROSS_COMPILE=${TOOLCHAIN}
+
+echo "Building Modules..."
+		make -j3 ARCH=arm CROSS_COMPILE=${TOOLCHAIN} modules
+
+	for module in "${MODULES[@]}" ; do
+			cp "${module}" ${MODULES_DIR}
+			${STRIP} --strip-unneeded ${MODULES_DIR}/*
+	done
+
+echo "Building CWM Kernel..."
+		cd ${KERNEL_DIR}
+		make -j3 ARCH=arm CROSS_COMPILE=${TOOLCHAIN} CONFIG_INITRAMFS_SOURCE=${KK_CWM_INITRAMFS_SOURCE}
+		cp arch/arm/boot/zImage ${OUTDIR}
+		cd ${OUTDIR}
+		echo "Creating KK CWM kernel zip..."
+		zip -r Render-KK-$CURRENTDATE-CWM.zip ./ -x *.zip *.gitignore
+
+echo "Building TWRP Kernel..."
+		cd ${KERNEL_DIR}
+		make -j3 ARCH=arm CROSS_COMPILE=${TOOLCHAIN} CONFIG_INITRAMFS_SOURCE=${KK_TWRP_INITRAMFS_SOURCE}
+		cp arch/arm/boot/zImage ${OUTDIR}
+		cd ${OUTDIR}
+		echo "Creating KK TWRP kernel zip..."
+		zip -r Render-KK-$CURRENTDATE-TWRP.zip ./ -x *.zip *.gitignore
+
+echo "Done!"
